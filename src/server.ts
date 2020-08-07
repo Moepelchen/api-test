ed3a605d8d0f58a9b6ac3722505a28cc5645f98c
Ulrich Tewes <u.tewes@comventure.de>
2020-08-07 13:49:22 +0200

    run on different port

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 var app = express();
-app.listen(3000, () => {
+app.listen(80, () => {
     console.log("Server running on port 3000");
 });
 app.use(bodyParser)
