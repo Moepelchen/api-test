2a2bc3d60eed8fc48aedde1d105daff82a672523
Ulrich Tewes <u.tewes@comventure.de>
2020-08-07 13:56:53 +0200

    run on different port

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,11 @@ app.use(bodyParser)
 app.get("/okta", (req, res, next) => {
     res.json({ "verification" : req.headers["x-okta-verification-challenge"] });
 });
+
+app.get("/", (req, res, next) => {
+    res.json({ "hello" : "world"});
+});
+
 app.post("/okta", (req, res, next) => {
     console.dir(req.body);
     res.render('blah.jade');
